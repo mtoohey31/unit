f1b08acf8852baf5f460b71cbb8dc5116c49daf3
Matthew Toohey <contact@mtoohey.com>
2023-03-03 23:53:23 -0500

    feat: initial commit

---
+++ flake.nix
@@ -0,0 +1,25 @@
+{
+  description = "CHANGEME";
+
+  inputs = {
+    nixpkgs.url = "nixpkgs/release-22.11";
+    utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, utils }: {
+    overlays.default = final: _: {
+      CHANGEME = final.stdenv.mkDerivation {
+        pname = "CHANGEME";
+        version = "0.1.0";
+        src = ./.;
+      };
+    };
+  } // utils.lib.eachDefaultSystem (system: with import nixpkgs
+    { overlays = [ self.overlays.default ]; inherit system; }; {
+    packages.default = CHANGEME;
+
+    devShells.default = mkShell {
+      packages = [ ponyc pony-corral ];
+    };
+  });
+}
